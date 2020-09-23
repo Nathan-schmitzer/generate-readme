1ecab8d044384489820a5fd4d586b734bf199320
Nathan Schmitzer <nathan.schmitzer@yahoo.com>
2020-09-23 13:02:15 -0500

    didnt add gitignore to my first repo so this will be my first commit on this one!

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# generate-readme
+# Generate readme file
+## Description: 
+This is an app that is used for easily creating professional readmes
+## Installation: 
+
+## Usage: 
+The use of this app is to help generate a profesional readme file, quickly and easily
+## Contributing: 
+
+## Feedback: 
+
+## License: 
+![License](https://img.shields.io/static/v1?label=License&message=MIT&color=critical)
+
+## Github username: 
+nathan-schmitzer
+## Email link: 
+nathan.schmitzer@yahoo.com
---
+++ index.js
@@ -0,0 +1,97 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+
+function promptUser() {
+    return inquirer.prompt([{
+        type: "input",
+        name: "title",
+        message: "What do you want the title of your README to be?"
+    }, {
+        type: "input",
+        name: "description",
+        message: "What do you want the description of your README to be?"
+    }, {
+        type: "input",
+        name: "installation",
+        message: "What are your instrucions for installation?"
+    }, {
+        type: "input",
+        name: "usage",
+        message: "What is the use of your app?"
+    }, {
+        type: "input",
+        name: "contributing",
+        message: "How to make contributions?"
+    }, {
+        type: "input",
+        name: "tests",
+        message: "How will you report issues?"
+    }, {
+        type: "list",
+        name: "license",
+        choices: ["MIT","Apache","Mozilla Public License"],
+        message: "What license will you be inputing?"
+    },{
+        type:"list",
+        name: "color",
+        message: "What color do you want your license to be?",
+        choices: ["brightgreen", "critical", "orange", "success", "ff69b4"]
+    }, {
+        type: "input",
+        name: "github",
+        message: "What is your github username?"
+    }, {
+        type: "input",
+        name: "email",
+        message: "What is your email address?"
+    }])
+};
+
+function generateREADME(answers) {
+
+    
+    return `# ${answers.title}
+## Description: 
+${answers.description}
+## Installation: 
+${answers.installation}
+## Usage: 
+${answers.usage}
+## Contributing: 
+${answers.contributing}
+## Feedback: 
+${answers.tests}
+## License: 
+![License](https://img.shields.io/static/v1?label=License&message=${answers.license}&color=${answers.color})
+
+## Github username: 
+${answers.github}
+## Email link: 
+${answers.email}`;
+    
+        
+
+};
+
+async function init() {
+    console.log("Hello from init!");
+    try {
+        const answers = await promptUser();
+
+        const README = generateREADME(answers);
+
+        await writeFileAsync("README.md", README);
+
+        console.log("Sucessfully wrote to README.md");
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+
+
+init();
